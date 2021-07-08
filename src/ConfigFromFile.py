25b24b190bf0d3b9aab2afe45c560a689e832603
Derek Somerville <47667752+DerekSomerville@users.noreply.github.com>
2021-07-08 10:23:36 +0100

    Fix working directory

--- src/ConfigFromFile.py
+++ src/ConfigFromFile.py
@@ -1,5 +1,13 @@
+import os
+
+def fixWorkingDirectory():
+    currentWorkingDirectory = os.getcwd()
+    while "test" in currentWorkingDirectory or "src" in currentWorkingDirectory:
+        os.chdir("../")
+        currentWorkingDirectory = os.getcwd()
 
 def getConfig():
+    fixWorkingDirectory()
     configPath = "resource/Config/properties.cfg"
     propertyFile = open(configPath,"r")
     propertyData = propertyFile.read().splitlines()
